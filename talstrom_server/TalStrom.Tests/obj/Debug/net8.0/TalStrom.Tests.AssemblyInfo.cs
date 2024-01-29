dfa1b0cc83f5a8f28aabfed9a5d174c3d38f9dc9
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-29 14:42:53 +0100

    add missing packages

--- talstrom_server/TalStrom.Tests/obj/Debug/net8.0/TalStrom.Tests.AssemblyInfo.cs
+++ talstrom_server/TalStrom.Tests/obj/Debug/net8.0/TalStrom.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TalStrom.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724c68cba9fabd3ec1e37494118b1456739c714e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9b76b7ae927bed35ec373254c992003434b9db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TalStrom.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TalStrom.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
