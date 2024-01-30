448667c6c7f39ba2c6f656d3e6c5601e109cfd01
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-30 15:02:03 +0100

    Frontend clean files

--- talstrom_web/src/ui/footer.tsx
+++ talstrom_web/src/ui/footer.tsx
@@ -4,26 +4,7 @@ import packageInfo from "../../package.json"
 export default function Footer() {
   return (
     <footer className={"w-full"}>
-      <hr />
-      <ul className={"w-full"}>
-        <li className={"w-full"}>
-          <a href="https://next-auth.js.org">Documentation</a>
-        </li>
-        <li className={"w-full"}>
-          <a href="https://www.npmjs.com/package/next-auth">NPM</a>
-        </li>
-        <li className={"w-full"}>
-          <a href="https://github.com/nextauthjs/next-auth-example">GitHub</a>
-        </li>
-        <li className={"w-full"}>
-          <Link href="/policy">
-            <p>Policy</p>
-          </Link>
-        </li>
-        <li className={"w-full"}>
-          <em>next-auth@{packageInfo.dependencies["next-auth"]}</em>
-        </li>
-      </ul>
+      <h2>Footer</h2>
     </footer>
   )
 }
