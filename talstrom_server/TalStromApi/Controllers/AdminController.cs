4a5421592eea42437868b4028ea03257d10306ec
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-30 15:00:13 +0100

    Update the controllers with exceptions

--- talstrom_server/TalStromApi/Controllers/AdminController.cs
+++ talstrom_server/TalStromApi/Controllers/AdminController.cs
@@ -9,99 +9,129 @@ using TalStromApi.Models;
 
 namespace TalStromApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AdminController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class AdminController : ControllerBase
+  {
+    private readonly TalStromDbContext _context;
+
+    public AdminController(TalStromDbContext context)
     {
-        private readonly TalStromDbContext _context;
-
-        public AdminController(TalStromDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Admin
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Admin>>> GetLogin()
-        {
-            return await _context.Admin.ToListAsync();
-        }
-
-        // GET: api/Admin/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Admin>> GetAdmin(int id)
-        {
-            var admin = await _context.Admin.FindAsync(id);
-
-            if (admin == null)
-            {
-                return NotFound();
-            }
-
-            return admin;
-        }
-
-        // PUT: api/Admin/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAdmin(int id, Admin admin)
-        {
-            if (id != admin.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(admin).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AdminExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Admin
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Admin>> PostAdmin(Admin admin)
-        {
-            _context.Admin.Add(admin);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAdmin", new { id = admin.Id }, admin);
-        }
-
-        // DELETE: api/Admin/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAdmin(int id)
-        {
-            var admin = await _context.Admin.FindAsync(id);
-            if (admin == null)
-            {
-                return NotFound();
-            }
-
-            _context.Admin.Remove(admin);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool AdminExists(int id)
-        {
-            return _context.Admin.Any(e => e.Id == id);
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Admin>>> GetLogin()
+    {
+      try
+      {
+        return await _context.Admin.ToListAsync();
+      }
+      catch (Exception ex)
+      {
+        // Log the exception for troubleshooting
+        Console.WriteLine($"Error in GetLogin: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Admin>> GetAdmin(int id)
+    {
+      try
+      {
+        var admin = await _context.Admin.FindAsync(id);
+
+        if (admin == null)
+        {
+          return NotFound("No Admin found");
+        }
+
+        return admin;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in GetAdmin(id): {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAdmin(int id, Admin admin)
+    {
+      if (id != admin.Id)
+      {
+        return BadRequest("Invalid request");
+      }
+
+      try
+      {
+        _context.Entry(admin).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+      }
+      catch (DbUpdateConcurrencyException ex)
+      {
+        Console.WriteLine($"Concurrency error in PutAdmin: {ex}");
+        if (!AdminExists(id))
+        {
+          return NotFound("Admin ID doesn't exist");
+        }
+        else
+        {
+          return StatusCode(500, "Internal Server Error");
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in PutAdmin: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Admin>> PostAdmin(Admin admin)
+    {
+      try
+      {
+        _context.Admin.Add(admin);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetAdmin", new { id = admin.Id }, admin);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in PostAdmin: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAdmin(int id)
+    {
+      try
+      {
+        var admin = await _context.Admin.FindAsync(id);
+        if (admin == null)
+        {
+          return NotFound();
+        }
+
+        _context.Admin.Remove(admin);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in DeleteAdmin: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    private bool AdminExists(int id)
+    {
+      return _context.Admin.Any(e => e.Id == id);
+    }
+  }
 }
--- talstrom_server/TalStromApi/Controllers/PostsController.cs
+++ talstrom_server/TalStromApi/Controllers/PostsController.cs
@@ -9,99 +9,123 @@ using TalStromApi.Models;
 
 namespace TalStromApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PostsController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class PostsController : ControllerBase
+  {
+    private readonly TalStromDbContext _context;
+
+    public PostsController(TalStromDbContext context)
     {
-        private readonly TalStromDbContext _context;
-
-        public PostsController(TalStromDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Posts
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Posts>>> GetPosts()
-        {
-            return await _context.Posts.ToListAsync();
-        }
-
-        // GET: api/Posts/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Posts>> GetPosts(int id)
-        {
-            var posts = await _context.Posts.FindAsync(id);
-
-            if (posts == null)
-            {
-                return NotFound();
-            }
-
-            return posts;
-        }
-
-        // PUT: api/Posts/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPosts(int id, Posts posts)
-        {
-            if (id != posts.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(posts).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PostsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Posts
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Posts>> PostPosts(Posts posts)
-        {
-            _context.Posts.Add(posts);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPosts", new { id = posts.Id }, posts);
-        }
-
-        // DELETE: api/Posts/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePosts(int id)
-        {
-            var posts = await _context.Posts.FindAsync(id);
-            if (posts == null)
-            {
-                return NotFound();
-            }
-
-            _context.Posts.Remove(posts);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool PostsExists(int id)
-        {
-            return _context.Posts.Any(e => e.Id == id);
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Posts>>> GetPosts()
+    {
+      try
+      {
+        return await _context.Posts.ToListAsync();
+      }
+      catch (Exception ex)
+      {
+        // Log the exception for troubleshooting
+        Console.WriteLine($"Error in GetPosts: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Posts>> GetPosts(int id)
+    {
+      try
+      {
+        var posts = await _context.Posts.FindAsync(id);
+
+        if (posts == null)
+        {
+          return NotFound();
+        }
+
+        return posts;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in GetPosts(id): {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPosts(int id, Posts posts)
+    {
+      if (id != posts.Id)
+      {
+        return BadRequest("Invalid request");
+      }
+
+      try
+      {
+        _context.Entry(posts).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+      }
+      catch (DbUpdateConcurrencyException ex)
+      {
+        // Handle concurrency exception
+        Console.WriteLine($"Concurrency error in PutPosts: {ex}");
+        return Conflict("Concurrency Error");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in PutPosts: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Posts>> PostPosts(Posts posts)
+    {
+      try
+      {
+        _context.Posts.Add(posts);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetPosts", new { id = posts.Id }, posts);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in PostPosts: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePosts(int id)
+    {
+      try
+      {
+        var posts = await _context.Posts.FindAsync(id);
+        if (posts == null)
+        {
+          return NotFound();
+        }
+
+        _context.Posts.Remove(posts);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in DeletePosts: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    private bool PostsExists(int id)
+    {
+      return _context.Posts.Any(e => e.Id == id);
+    }
+  }
 }
