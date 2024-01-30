86d9bcd43d026ee92605fb378d51baeb7157388a
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-30 14:01:54 +0100

    Update Users model

--- talstrom_server/TalStromApi/Models/User.cs
+++ talstrom_server/TalStromApi/Models/User.cs
@@ -1,10 +1,17 @@
 namespace TalStromApi.Models;
 
+public enum Role {
+  Customer,
+  Developer
+}
+
 public class User
 {
   public int Id { get; set; }
   public string Name { get; set; }
+  public Role Role { get; set; }
   public string Password { get; set; }
   public List<string> Videos { get; set; }
-
+  public List<User>? Developers { get; set; }
+  public List<Posts> Posts { get; set; } 
 }
