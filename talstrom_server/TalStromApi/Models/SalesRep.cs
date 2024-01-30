9850926270f836457db430300fd5949d019a0099
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-30 14:01:45 +0100

    Add SalesRep model

---
+++ talstrom_server/TalStromApi/Models/SalesRep.cs
@@ -0,0 +1,9 @@
+namespace TalStromApi.Models;
+
+public class SalesRep
+{
+  public int Id { get; set; }
+  public string FullName { get; set; }
+  public string Email { get; set; }
+  public string PhoneNumber { get; set; }
+}
