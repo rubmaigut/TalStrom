95b6bd3a0b6f04a1f5ea9eaefa537e5392d27b61
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-30 13:43:42 +0100

    Add Video model

---
+++ talstrom_server/TalStromApi/Models/Video.cs
@@ -0,0 +1,38 @@
+namespace TalStromApi.Models;
+
+public class Video
+{
+  public string Title { get; }
+  public int Duration { get; }
+  public Tuple<int, int> Resolution { get; }
+  public float FrameRate { get; }
+  public string FileFormat { get; }
+  public byte[] ByteData { get; }
+
+  public Video(string title, int duration, Tuple<int, int> resolution, float frameRate, string fileFormat,
+    byte[] byteData)
+
+  {
+    Title = title;
+    Duration = duration;
+    FrameRate = frameRate;
+    FileFormat = fileFormat;
+    ByteData = byteData;
+  }
+
+  public void Play()
+  {
+    Console.WriteLine($"Playing: {Title}");
+  }
+
+  public void Save(string filePath)
+  {
+    File.WriteAllBytes(filePath, ByteData);
+    Console.WriteLine($"Video save to: {filePath}");
+  }
+
+  public void ProcessFrames()
+  {
+    Console.WriteLine($"ProcessFrames for: {Title}");
+  }
+}
