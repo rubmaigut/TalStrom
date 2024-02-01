c16cbeb4818879d64f08f5d46e5c46ad0c723492
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-01 06:16:22 +0100

    update main index

--- talstrom_web/src/pages/index.tsx
+++ talstrom_web/src/pages/index.tsx
@@ -1,55 +1,14 @@
-import Link from "next/link";
-import Layout from "../ui/layout";
+import Header from "@/ui/header";
 import { useSession } from "next-auth/react";
-import SignIn from "@/ui/sign-in";
-import jwt from "./api/endpoints/jwt";
-import { useEffect, useState } from "react";
-import SuccessLogin from "@/ui/sucess-login";
 
 export default function Page() {
   const { data: session } = useSession();
-  const [jwtData, setJwtData] = useState(null);
-
-  useEffect(() => {
-    const fetchJwt = async () => {
-      if (session) {
-        const response = await fetch("http://localhost:3000/api/endpoints/jwt");
-        const data = await response.json();
-        setJwtData(data);
-      }
-    };
-    fetchJwt();
-  }, [session]);
-
-  if (!session || !jwtData) {
-    return (
-      <section>
-        <h2>SignIn</h2>
-        <SignIn />
-      </section>
-    );
-  }
-
-  const userInfo = session.user
-    ? {
-        email: session.user.email || "",
-        name: session.user.name || "",
-        image: session.user.image || "",
-      }
-    : null;
-
+  
   return (
-    <div>
-      {userInfo && jwtData && (
-        <SuccessLogin user={userInfo} jwt={jwtData} />
-      )}
-      <div>
-          {session && "role === Admin" && (
-            <Layout>
-            <Link href="/admin/invite">Admin Portal</Link>
-            </Layout>
-          )}
-      </div>
-    </div>
+    <>
+    <Header/>
+    
+    </>
+     
   );
 }
