a01dea52e07da189ddec4a1ed58c49ee49740eb9
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-01 18:43:42 +0100

    add next config

--- talstrom_web/next.config.mjs
+++ talstrom_web/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  publicRuntimeConfig: {
+    apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 };
 
 export default nextConfig;
