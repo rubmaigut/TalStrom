74f9fb08553497d2e85cb96b24c7764640e35499
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-01 13:17:18 +0100

    Add delete video from blob container

--- talstrom_server/TalStromApi/Services/BlobStorageService.cs
+++ talstrom_server/TalStromApi/Services/BlobStorageService.cs
@@ -5,22 +5,30 @@ namespace AzureFullstackPractice.Data;
 
 public class BlobStorageService
 {
-    private readonly BlobServiceClient _client;
+  private readonly BlobServiceClient _client;
 
-    public BlobStorageService(BlobServiceClient client)
-    {
-        _client = client;
-    }
+  public BlobStorageService(BlobServiceClient client)
+  {
+    _client = client;
+  }
 
-    public async Task UploadFileAsync(string containerName, string filePath, string userSub)
-    {
-        var blobContainer = _client.GetBlobContainerClient(containerName);
-        await blobContainer.CreateIfNotExistsAsync();
-        var blobClient = blobContainer.GetBlobClient(Path.GetFileName(filePath));
-        await blobClient.UploadAsync(filePath, true);
+  public async Task UploadFileAsync(string containerName, string filePath, string userSub)
+  {
+    var blobContainer = _client.GetBlobContainerClient(containerName);
+    await blobContainer.CreateIfNotExistsAsync();
+    var blobClient = blobContainer.GetBlobClient(Path.GetFileName(filePath));
+    await blobClient.UploadAsync(filePath, true);
 
-        Dictionary<string, string> metadata = new Dictionary<string, string>();
-        metadata.Add("userId", userSub);
-        blobClient.SetMetadata(metadata);
-    }
+    Dictionary<string, string> metadata = new Dictionary<string, string>();
+    metadata.Add("userId", userSub);
+    blobClient.SetMetadata(metadata);
+  }
+
+  public async Task deleteFileAsync(string containerName, string filePath)
+  {
+    var blobContainer = _client.GetBlobContainerClient(containerName);
+    await blobContainer.CreateIfNotExistsAsync();
+    var blobClient = blobContainer.GetBlobClient(Path.GetFileName(filePath));
+    await blobClient.DeleteAsync();
+  }
 }
