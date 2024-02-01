67a417e915e22d878144dc62b7df1945d26d0e21
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-02-01 14:29:15 +0100

    add: Error handling

--- talstrom_server/TalStromApi/Controllers/VideoControlller.cs
+++ talstrom_server/TalStromApi/Controllers/VideoControlller.cs
@@ -8,51 +8,58 @@ namespace TalStromApi.Controllers;
 [Route("[controller]")]
 public class VideoController : ControllerBase
 {
-  private readonly TalStromDbContext _context;
-  private readonly BlobStorageService _client;
+    private readonly TalStromDbContext _context;
+    private readonly BlobStorageService _client;
 
-  public VideoController(TalStromDbContext context, BlobStorageService client)
-  {
-    _context = context;
-    _client = client;
-  }
-
-  [HttpGet]
-  public async Task<ActionResult<Video>> GetAllVideos()
-  {
-    var videos = await _client.GetAllVideos("movies");
-    return Ok(videos);
-  }
-
-  [HttpPost("upload")]
-  public async Task<IActionResult> Upload([FromForm] IFormFile file, string userSub)
-  {
-    if (file == null || file.Length == 0)
+    public VideoController(TalStromDbContext context, BlobStorageService client)
     {
-      return BadRequest("No file uploaded.");
+        _context = context;
+        _client = client;
     }
 
-    var fileName = Guid.NewGuid();
-    using (var stream = System.IO.File.Create($"{fileName}.mp4"))
+    [HttpGet]
+    public async Task<ActionResult<Video>> GetAllVideos()
     {
-      await file.CopyToAsync(stream);
+        try
+        {
+            var videos = await _client.GetAllVideos("movies");
+            return Ok(videos);
+        }
+        catch (Exception e)
+        {
+            return NotFound(e);
+        }
     }
 
-    //Be specific about file format for now.
-    await _client.UploadFileAsync("movies", $"{fileName}.mp4", userSub);
-    System.IO.File.Delete($"{fileName}.mp4");
-    return Ok("File uploaded successfully.");
-  }
-
-  [HttpDelete("delete")]
-  public async Task<IActionResult> Delete(string videoName)
-  {
-    if (videoName == null)
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload([FromForm] IFormFile file, string userSub)
     {
-      return BadRequest("File not found.");
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+
+        var fileName = Guid.NewGuid();
+        using (var stream = System.IO.File.Create($"{fileName}.mp4"))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        //Be specific about file format for now.
+        await _client.UploadFileAsync("movies", $"{fileName}.mp4", userSub);
+        System.IO.File.Delete($"{fileName}.mp4");
+        return Ok("File uploaded successfully.");
     }
 
-    await _client.DeleteFileAsync("movies", videoName);
-    return Ok("File deleted.");
-  }
+    [HttpDelete("delete")]
+    public async Task<IActionResult> Delete(string videoName)
+    {
+        if (videoName == null)
+        {
+            return BadRequest("File not found.");
+        }
+
+        await _client.DeleteFileAsync("movies", videoName);
+        return Ok("File deleted.");
+    }
 }
