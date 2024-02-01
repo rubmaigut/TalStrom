a50cd3ff8a1b09f0cb3b6705a5d607a07fe68004
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-01 06:15:55 +0100

    update tailwind setup

--- talstrom_web/src/pages/_app.tsx
+++ talstrom_web/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
-import "../styles/styles.css"
+import "../styles/global.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
