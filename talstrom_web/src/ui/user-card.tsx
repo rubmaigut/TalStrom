cc23fc66a8b97e71c71f8bbf6d2b11efd0bd98ee
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-03 13:10:04 +0100

    Add user card component

---
+++ talstrom_web/src/ui/user-card.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { User } from '@/types/IUser';
+import Image from 'next/image';
+
+interface UserCardProps {
+  user: User;
+}
+
+const formatDate = (dateString: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+const userCard: React.FC<UserCardProps> = ({ user }) => (
+  <>
+    <p>
+      <Image src={user.picture} alt="Profile" width={96} height={96} />
+    </p>
+    <p>ID: {user.id}</p>
+    <p>Name: {user.name}</p>
+    <p>Email: {user.email}</p>
+    <p>Role: {user.role}</p>
+    <p>Phone Number: {user.phoneNumber || 'Not available'}</p>
+    <p>Date Added: {formatDate(user.dateAdded)}</p>
+    <p>Followers: {user.followers ? 'Yes' : 'No'}</p>
+    <p>Posts: {user.posts ? 'Yes' : 'No'}</p>
+  </>
+);
+
+export default userCard;
