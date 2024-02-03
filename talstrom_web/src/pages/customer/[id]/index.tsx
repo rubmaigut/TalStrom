0657da3174f526b6c661978b4a26aeb4a26be83d
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-03 13:08:29 +0100

    Add usr profile page

--- talstrom_web/src/pages/customer/[id]/index.tsx
+++ talstrom_web/src/pages/customer/[id]/index.tsx
@@ -1,11 +1,55 @@
 import SignIn from '@/ui/sign-in';
 import { useSession } from 'next-auth/react';
-import CustomerProfilePage from './customerProfilePage';
+import { useEffect, useState } from 'react';
+import { fetchUserById } from '@/lib/data';
+import { User } from '@/types/IUser';
+import UserCard from '../../../ui/user-card';
+import NavLinks, { links } from '@/ui/customer/nav-links';
+import UserFindMatch from '../[id]/find-match/index';
+import UserMyDevs from '../[id]/my-devs/index';
+import UserPost from '../[id]/post/index';
+import UserSaved from '../[id]/saved/index';
 
-export default function Page() {
+export default function UserProfilePage() {
   const { data: session } = useSession();
+  const [user, setUser] = useState<User | null>(null);
+  const [activeLink, setActiveLink] = useState<string>('posts');
 
-  console.log();
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        if (session) {
+          const sub = session.user?.sub || '';
+          const userData = await fetchUserById(sub);
+          setUser(userData);
+        }
+      } catch (error) {
+        console.error('Failed to fetch user data:', error);
+      }
+    };
+
+    loadUser();
+  }, [session]);
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
+
+  const renderPageContent = () => {
+    switch (activeLink) {
+      case 'posts':
+        return <UserPost />;
+      case 'find-match':
+        return <UserFindMatch />;
+      case 'my-devs':
+        return <UserMyDevs />;
+      case 'saved':
+        return <UserSaved />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       {!session ? (
@@ -14,7 +58,18 @@ export default function Page() {
         </section>
       ) : (
         <>
-          <CustomerProfilePage />
+          <div>
+            {user ? (
+              <>
+                <p>user Profile</p>
+                <UserCard user={user} />
+                <NavLinks onLinkClick={handleLinkClick} />
+                <div>{renderPageContent()}</div>
+              </>
+            ) : (
+              <p>Loading user data...</p>
+            )}
+          </div>
         </>
       )}
     </>
