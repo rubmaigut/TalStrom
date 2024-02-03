9c94d8932c16ad704ebf251c9003b2f222912e26
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-03 15:55:23 +0100

    add icons

--- talstrom_web/src/ui/dashboard/users-summary.tsx
+++ talstrom_web/src/ui/dashboard/users-summary.tsx
@@ -1,3 +1,4 @@
+import { UserGroupIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 interface SummaryProps {
@@ -14,7 +15,9 @@ const UserSummary: React.FC<SummaryProps> = ({totalUser, deletedUser}) => {
           <h3 className="font-bold">Total Users</h3>
           <p className="text-gray-500">{totalUser}</p>
         </div>
-        <i className="fa-solid fa-users p-4 bg-gray-200 rounded-md"></i>
+        <i className="fa-users p-2 bg-gray-200 rounded-md">
+          <UserGroupIcon className="w-6"/>
+        </i>
       </div>
 
       <div className="bg-slate-50 p-5 m-2 flex justify-between items-center shadow">
@@ -22,7 +25,9 @@ const UserSummary: React.FC<SummaryProps> = ({totalUser, deletedUser}) => {
           <h3 className="font-bold">Deleted Users</h3>
           <p className="text-gray-500">{deletedUser}</p>
         </div>
-        <i className="fa-solid fa-users p-4 bg-red-200 rounded-md"></i>
+        <i className="p-2 bg-red-200 rounded-md">
+        <UserGroupIcon className="w-6"/>
+        </i>
       </div>
     </div>
     </div>
