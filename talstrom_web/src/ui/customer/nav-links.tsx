5cf1b7701244d92cd1146a4e79841e19337a8bc0
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-04 11:57:32 +0100

    refactor nav-links Add interface

--- talstrom_web/src/ui/customer/nav-links.tsx
+++ talstrom_web/src/ui/customer/nav-links.tsx
@@ -1,4 +1,5 @@
 import { usePathname } from 'next/navigation';
+
 import Link from 'next/link';
 import clsx from 'clsx';
 import {
@@ -8,19 +9,32 @@ import {
   StarIcon as SolidStarIcon,
 } from '@heroicons/react/24/outline';
 
+interface Link {
+  name: string;
+  href: string;
+  icon: React.ComponentType<
+    React.SVGProps<SVGSVGElement> & { ref?: React.Ref<SVGSVGElement> }
+  >;
+}
+
 interface NavLinksProps {
   onLinkClick: (link: string) => void;
 }
 
+
 export const links = [
-  { name: 'Posts', href: '/customer/[id]/post', icon: NewspaperIcon },
+  { name: "posts", href: "/customer/[id]/post", icon: NewspaperIcon },
   {
-    name: 'Find Match',
-    href: '/customer/[id]/find-match',
+    name: "find-match",
+    href: "/customer/[id]/find-match",
     icon: MagnifyingGlassIcon,
   },
-  { name: 'My Devs', href: '/customer/[id]/my-devs', icon: UserGroupIcon },
-  { name: 'Saved', href: '/customer/[id]/saved', icon: SolidStarIcon },
+  {
+    name: 'My Devs',
+    href: `/customer/id/my-devs`,
+    icon: UserGroupIcon,
+  },
+  { name: "saved", href: "/customer/[id]/saved", icon: SolidStarIcon },
 ];
 
 const NavLinks: React.FC<NavLinksProps> = ({ onLinkClick }) => {
@@ -41,10 +55,10 @@ const NavLinks: React.FC<NavLinksProps> = ({ onLinkClick }) => {
             key={link.name}
             onClick={() => handleLinkClick(link.name)}
             className={clsx(
-              'flex h-[48px] items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              "flex h-[48px] items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                'bg-sky-100 text-teal-600': pathname === link.href,
-              },
+                "bg-sky-100 text-teal-600": pathname === link.href,
+              }
             )}
           >
             <IconComponent className="w-6" />
