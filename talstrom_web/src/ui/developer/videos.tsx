f3600ff1560cfeeeb6e2bd6434ffcd9c56f3aff0
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-02-04 02:38:07 +0100

    add: grid styling

--- talstrom_web/src/ui/developer/videos.tsx
+++ talstrom_web/src/ui/developer/videos.tsx
@@ -10,11 +10,11 @@ const VideosGrid: FC<VideosGridProps> = ({ videos }) => {
     <section>
       <p className="m-2">Video Posts</p>
       {videos && videos.length && (
-        <div className="grid grid-cols-4 my-2 md:grid-cols-5 lg:grid-cols-6">
+        <div className="grid grid-cols-4 my-2 md:grid-cols-6 m-2 md:mx-32">
           {videos.map((video, i) => {
             return (
               
-              <video key={i} className="rounded-xl object-cover mx-auto h-[10em] sm:h-[15em] w-[12em]"  loop muted src={video.uri} />
+              <video key={i} className="object-cover mx-auto h-[10em] sm:h-[15em] w-[12em]"  loop muted src={video.uri} />
             );
           })}
         </div>
