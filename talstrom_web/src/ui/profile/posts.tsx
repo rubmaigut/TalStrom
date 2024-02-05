8289abb38a5a9c1f5eb31f4347eba85cf4cb10de
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-05 08:14:51 +0100

    Refactor posts component

--- talstrom_web/src/ui/profile/posts.tsx
+++ talstrom_web/src/ui/profile/posts.tsx
@@ -1,26 +1,30 @@
-import { FC } from "react";
+import { Post } from '@/types/Posts';
+import { FC } from 'react';
 
 type PostsProps = {
-    posts: Post[] | undefined;
-}
-const Posts : FC<PostsProps> = ({posts}) => {
+  posts: Post[];
+};
+const UserPosts: FC<PostsProps> = ({ posts }) => {
+  console.log(posts);
 
-    console.log(posts);
-
-    if(posts && posts.length) return (
+  if (posts && posts.length) {
+    return (
       <div>
         <p>Video Posts</p>
-        {posts.map((post, i) =>  {
-            return (
-            <div key={`post-${i}`} className="container mx-auto my-2 p-2 border border-black rounded-md">
-              <h1>{post.title}</h1>
-              <p>{post.content}</p>
-            </div>
-            )
-        })}
+        {posts.map((post, i) => (
+          <div
+            key={`post-${i}`}
+            className="container mx-auto my-2 p-2 border border-black rounded-md"
+          >
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+          </div>
+        ))}
       </div>
     );
-  };
+  } else {
+    return <p>No posts available.</p>;
+  }
+};
 
-
-  export default Posts;
+export default UserPosts;
