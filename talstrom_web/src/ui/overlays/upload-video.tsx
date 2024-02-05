49a556dff736d746d4afff2d5682446490df9c8d
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-02-05 09:31:12 +0100

    add: styling to upload-video add: centering of video in videoplayer

--- talstrom_web/src/ui/overlays/upload-video.tsx
+++ talstrom_web/src/ui/overlays/upload-video.tsx
@@ -42,7 +42,7 @@ const UploadContainer = ({ closeWindow, sub }: UploadContainerProps) => {
   };
 
   const uploadVideoToCloud = async () => {
-    if (!file) { 
+    if (!file) {
       return;
     }
 
@@ -51,13 +51,17 @@ const UploadContainer = ({ closeWindow, sub }: UploadContainerProps) => {
   };
 
   return (
-    <section className="absolute z-50 w-96 left-0 md:left-20 top-0 mt-[80px] mb-[40px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4">
-      <button onClick={closeWindow}>
+    <div
+      id="upload-video-overlay"
+      className="fixed flex justify-center align-middle items-center z-10 left-0 top-0 w-full h-screen bg-white overflow-auto"
+    >
+      <div className="container w-[300px] top-12 md:top-40">
+      <button className="mx-8" onClick={closeWindow}>
         <GiCancel size="30" />
       </button>
       <div>
-        <h1 className="text-[23px] font-semibold">Upload video</h1>
-        <h2 className="text-gray-400 mt-1">Post a video to your account</h2>
+        <h1 className="text-[23px] font-semibold my-2 mx-8">Upload video</h1>
+        <h2 className="text-gray-400 mt-1 mx-8">Post a video to your account</h2>
       </div>
 
       <div className="mt-8 md:flex flex-col gap-6 max-w-80 items-center justify-center">
@@ -193,7 +197,153 @@ const UploadContainer = ({ closeWindow, sub }: UploadContainerProps) => {
           <div className="text-red-400 mt-4">{error.message}</div>
         ) : null}
       </div>
-    </section>
+      </div>
+      
+    </div>
+
+    // <section className="absolute z-50 w-96 left-0 md:left-20 top-0 mt-[80px] mb-[40px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4">
+    //   <button onClick={closeWindow}>
+    //     <GiCancel size="30" />
+    //   </button>
+    //   <div>
+    //     <h1 className="text-[23px] font-semibold">Upload video</h1>
+    //     <h2 className="text-gray-400 mt-1">Post a video to your account</h2>
+    //   </div>
+
+    //   <div className="mt-8 md:flex flex-col gap-6 max-w-80 items-center justify-center">
+    //     {!fileDisplay ? (
+    //       <label
+    //         htmlFor="input"
+    //         className="
+    // md:mx-0
+    // mx-auto
+    // mt-4
+    // mb-6
+    // flex
+    // flex-col
+    // items-center
+    // justify-center
+    // w-full
+    // max-w-[260px]
+    // h-[470px]
+    // text-center
+    // p-3
+    // border-2
+    // border-dashed
+    // border-gray-300
+    // rounded-lg
+    // hover:bg-gray-100
+    // cursor-pointer
+    // "
+    //       >
+    //         <div className="text-[#b3b3b3] w-1/3 mx-auto">
+    //           <CloudArrowUpIcon />
+    //         </div>
+    //         <p className="mt-4  text-[17px]">Select video to upload</p>
+    //         <p className="mt-1.5 text-gray-500 text-[13px]">
+    //           Or drag and drop file
+    //         </p>
+    //         <p className="mt-12 text-gray-400 text-sm">MP4</p>
+    //         <p className="mt-2 text-gray-400 text-[13px]">Less than 600MB</p>
+    //         <label
+    //           htmlFor="fileInput"
+    //           className="px-2 py-1.5 mt-8 text-white text-[15px] w-[80%] bg-[#14b8a6] rounded-sm cursor-pointer"
+    //         >
+    //           Select File
+    //         </label>
+    //         <input
+    //           id="fileInput"
+    //           onChange={uploadHandler}
+    //           hidden
+    //           accept=".mp4"
+    //           capture={"environment"}
+    //           type="file"
+    //         />
+    //       </label>
+    //     ) : (
+    //       <div
+    //         className="
+    //       md:mx-0
+    //       mx-auto
+    //       mt-4
+    //       md:mb-12
+    //       mb-16
+    //       flex
+    //       items-center
+    //       justify-center
+    //       w-full
+    //       max-w-[260px]
+    //       h-[470px]
+    //       p-3
+    //       border-2
+    //     border-solid
+    //     border-gray-300
+    //       rounded-lg
+    //       cursor-pointer
+    //       relative"
+    //       >
+    //         {isUploading ? (
+    //           <div className="absolute flex items-center justify-center z-20 bg-black h-full w-full rounded-lg bg-opacity-50">
+    //             <div className="mx-auto flex items-centerjustify-center gap-1">
+    //               <BiLoaderCircle
+    //                 className="animate-spin"
+    //                 color="#f12b56"
+    //                 size={30}
+    //               />
+    //               <div className="text-white font-bold">Uploading...</div>
+    //             </div>
+    //           </div>
+    //         ) : null}
+
+    //         <video
+    //           className="absolute rounded-xl object-cover z-10 p-[13px] w-full-h-full"
+    //           autoPlay
+    //           loop
+    //           muted
+    //           src={fileDisplay}
+    //         />
+
+    //         <div className="absolute -bottom-12 flex items-center justify-between z-50 rounded-xl border w-full p-2 border-gray-300">
+    //           <div className="flex items-center truncate">
+    //             <BiCheckCircle size="16" className="min-w-[16px]" />
+    //             <p className="text-[11px] pl-1 truncate text-ellipsis">
+    //               {file ? file.name : ""}
+    //             </p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     )}
+    //     <div className="flex items-center justify-center mt-4 mb-6">
+    //       <button
+    //         disabled={isUploading}
+    //         onClick={() => discardVideoHandler()}
+    //         className="px-10 py-2.5 mt-2 mx-1 border text-black bg-white rounded-sm"
+    //       >
+    //         Discard
+    //       </button>
+    //       <button
+    //         disabled={isUploading}
+    //         onClick={() => uploadVideoToCloud()}
+    //         className={`px-10 py-2.5 mt-2 mx-1 border text-white ${
+    //           file && fileDisplay.length ? "bg-[#14b8a6]" : "bg-gray-300"
+    //         } rounded-sm`}
+    //       >
+    //         {isUploading ? (
+    //           <BiLoaderCircle
+    //             className="animate-spin"
+    //             color="#f12b56"
+    //             size={30}
+    //           />
+    //         ) : (
+    //           "Upload"
+    //         )}
+    //       </button>
+    //     </div>
+    //     {error ? (
+    //       <div className="text-red-400 mt-4">{error.message}</div>
+    //     ) : null}
+    //   </div>
+    // </section>
   );
 };
 
