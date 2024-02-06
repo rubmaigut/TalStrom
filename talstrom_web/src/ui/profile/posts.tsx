5b23216d68e1825bc984463a8fb9289912c01028
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-06 23:10:57 +0100

    Refactor the posts component Add overlay

--- talstrom_web/src/ui/profile/posts.tsx
+++ talstrom_web/src/ui/profile/posts.tsx
@@ -1,5 +1,6 @@
 import { updateUserPost } from '@/lib/data';
 import React, { useState } from 'react';
+import PostOverlay from '../overlays/post-viewer';
 
 type Post = {
   id: number;
@@ -29,6 +30,7 @@ const UserPost: React.FC<PostsProps> = ({ posts }) => {
     if (selectedPost) {
       try {
         await updateUserPost(selectedPost.id, editedContent);
+
         setEditMode(false);
         setSelectedPost(null);
       } catch (error) {
@@ -55,49 +57,30 @@ const UserPost: React.FC<PostsProps> = ({ posts }) => {
               ? 'bg-green-100'
               : 'bg-white-100'
           }`}
+          onClick={() => handlePostEditClick(post)}
         >
           <p className="text-sm text-gray-500">{post.postType}</p>
           <h1 className="text-xl font-bold mb-2">{post.title}</h1>
-          {isEditMode ? (
-            <textarea
-              className="w-full h-32 p-2 border border-gray-300 rounded-md"
-              value={editedContent}
-              onChange={(e) => setEditedContent(e.target.value)}
-            />
-          ) : (
-            <p>{post.content}</p>
-          )}
           <div className="mt-4 flex items-center justify-between">
             {post.author && post.author !== 'string' && (
               <p className="text-gray-600">{post.author}</p>
             )}
             <p className="text-gray-600">{post.createdDate}</p>
           </div>
-          {isEditMode ? (
-            <div className="mt-4 flex items-center space-x-2">
-              <button
-                className="px-4 py-2 text-white bg-green-500 rounded-md"
-                onClick={handlePostSaveClick}
-              >
-                Save
-              </button>
-              <button
-                className="px-4 py-2 text-white bg-red-500 rounded-md"
-                onClick={handlePostCancelClick}
-              >
-                Cancel
-              </button>
-            </div>
-          ) : (
-            <button
-              className="mt-4 px-4 py-2 text-white bg-blue-500 rounded-md"
-              onClick={() => handlePostEditClick(post)}
-            >
-              Edit
-            </button>
-          )}
         </div>
       ))}
+
+      {selectedPost && (
+        <PostOverlay
+          post={selectedPost}
+          isEditMode={isEditMode}
+          editedContent={editedContent}
+          onEditClick={() => setEditMode(true)}
+          onSaveClick={handlePostSaveClick}
+          onCancelClick={handlePostCancelClick}
+          onContentChange={setEditedContent}
+        />
+      )}
     </div>
   );
 };
