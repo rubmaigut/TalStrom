bba00960ff5aa9c72ee25bbad470b991205f0c8b
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-07 15:04:07 +0100

    update styles sideBar

--- talstrom_web/src/ui/customer/dashboard/nav-links.tsx
+++ talstrom_web/src/ui/customer/dashboard/nav-links.tsx
@@ -37,7 +37,7 @@ export default function CustomerNavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-200 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3 md:mb-1",
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3 md:mb-1",
               {
                 "bg-teal-100 text-teal-600": pathname === link.href,
               }
