3ffaf986e52da4156bf39973ff3a4858a0f2061f
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-07 21:02:36 +0100

    Styling for save component

--- talstrom_web/src/ui/atoms/profile/saved.tsx
+++ talstrom_web/src/ui/atoms/profile/saved.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CgArrowRightO } from 'react-icons/cg';
 
 const UserSaved = () => {
   const hardcodedUsers: {
@@ -46,14 +47,13 @@ const UserSaved = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center flex-col">
-      <p>Saved</p>
+    <div className="flex items-center justify-center flex-col mt-10">
       {hardcodedUsers.map((user) => (
         <div
           key={user.id}
           className="flex flex-col justify-center space-x-2 md:space-x-4 w-full md:w-3/5 pb-4"
         >
-          <div className="flex">
+          <div className="flex items-center justify-center">
             <div className="flex-shrink-0">
               <img
                 src={user.picture}
@@ -70,6 +70,9 @@ const UserSaved = () => {
               <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                 {user.email}
               </p>
+            </div>
+            <div className="flex pr-2">
+              <CgArrowRightO size={25} />
             </div>
           </div>
         </div>
