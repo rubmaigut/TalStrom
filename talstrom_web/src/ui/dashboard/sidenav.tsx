61a6cc720553ff01254ffa6ea196ef8534a469e2
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-08 00:10:49 +0100

    fix sign out bug

--- talstrom_web/src/ui/dashboard/sidenav.tsx
+++ talstrom_web/src/ui/dashboard/sidenav.tsx
@@ -9,6 +9,16 @@ import Image from "next/image";
 export default function SideNav() {
   const { userContextG } = useUser();
 
+  const handleSignOut = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+      await signOut({ redirect: false, callbackUrl: "/" });
+      window.location.href = "/";
+    } catch (error) {
+      console.error("Error signing out", error);
+    }
+  };
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -34,10 +44,7 @@ export default function SideNav() {
         <Link
           href={`/api/auth/signout`}
           className="flex grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          onClick={(e) => {
-            e.preventDefault();
-            signOut();
-          }}
+          onClick={handleSignOut}
         >
           <PowerIcon className="w-6" />
           <p className="hidden md:block">Sign out</p>
