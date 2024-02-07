d4cae8b52cc102ff97cc721331e1efa3f03a9f64
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-07 14:00:38 +0100

    fix icon mobile view

--- talstrom_web/src/ui/dashboard/sidenav.tsx
+++ talstrom_web/src/ui/dashboard/sidenav.tsx
@@ -33,14 +33,14 @@ export default function SideNav() {
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <Link
           href={`/api/auth/signout`}
-          className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          className="flex grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3"
           onClick={(e) => {
             e.preventDefault();
             signOut();
           }}
         >
           <PowerIcon className="w-6" />
-          Sign out
+          <p className="hidden md:block">Sign out</p>
         </Link>
       </div>
     </div>
