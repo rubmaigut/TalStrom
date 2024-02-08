43e87c87711a8efe861d9daaa53c50a23d9a33b0
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-08 22:58:42 +0100

    update nav-link ui

--- talstrom_web/src/ui/developer/nav-links.tsx
+++ talstrom_web/src/ui/developer/nav-links.tsx
@@ -1,5 +1,4 @@
 import { usePathname } from 'next/navigation';
-import Link from 'next/link';
 import clsx from 'clsx';
 import {
   NewspaperIcon,
@@ -8,6 +7,9 @@ import {
   BriefcaseIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
+import TalstromLogo from "@/ui/atoms/general ui/talstrom-logo";
+import LogoutButton from "@/ui/atoms/profile/log-out";
+import React from "react";
 
 interface NavLinksProps {
   onLinkClick: (link: string) => void;
@@ -35,25 +37,32 @@ const NavLinks: React.FC<NavLinksProps> = ({ onLinkClick }) => {
   };
 
   return (
-    <div className="flex gap-2 justify-around">
-      {links.map((link) => {
+    <div className="w-full flex fixed bg-gray-100 py-1 md:px-8">
+      <TalstromLogo shortVersion={true}/>
+      <div className="flex pl-4 gap-2 justify-between ">
+       {links.map((link) => {
         const IconComponent = link.icon;
         return (
           <div
             key={link.name}
             onClick={() => handleLinkClick(link.name)}
             className={clsx(
-              'flex items-center justify-center gap-2 rounded-lg bg-gray-200 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 lg:flex-none lg:justify-start lg:p-2 lg:px-3 custom-justify-center',
+              'flex items-center text-primary-light  justify-center gap-2 rounded-lg bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 lg:flex-none lg:justify-start lg:p-2 lg:px-3 custom-justify-center',
               {
-                'bg-teal-100 text-teal-600': pathname === link.href,
+                'bg-teal-100 text-tea-900': pathname === link.href,
               },
             )}
           >
             <IconComponent className="w-6" />
             <p className="hidden md:block">{link.name}</p>
+            
           </div>
         );
       })}
+        <div className="hidden md:block">
+        <LogoutButton/>
+        </div>
+      </div>
     </div>
   );
 };
