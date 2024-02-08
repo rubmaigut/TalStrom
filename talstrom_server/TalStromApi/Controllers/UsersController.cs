554ff76f011162ce3a150a93979bc03db01f7cfd
Davit Danielyan <davit-danielyan@hotmail.com>
2024-02-08 12:36:08 +0100

    Add favorite developer backend controller

--- talstrom_server/TalStromApi/Controllers/UsersController.cs
+++ talstrom_server/TalStromApi/Controllers/UsersController.cs
@@ -42,13 +42,12 @@ namespace TalStromApi.Controllers
     {
       try
       {
-        var user = await _context.User.
-          Include(ctx => ctx.Posts)
+        var user = await _context.User.Include(ctx => ctx.Posts)
           .Include(ctx => ctx.Videos)
           .Include(ctx => ctx.Images)
-          .FirstOrDefaultAsync(x=> x.Sub == sub);
-        
-       return user is null ? NotFound() :  Ok(user);
+          .FirstOrDefaultAsync(x => x.Sub == sub);
+
+        return user is null ? NotFound() : Ok(user);
       }
       catch (Exception ex)
       {
@@ -56,7 +55,7 @@ namespace TalStromApi.Controllers
         return StatusCode(500, "Internal Server Error");
       }
     }
-    
+
     [HttpGet("technologies/filter/{sub}")]
     public async Task<ActionResult<User>> GetUserByFilter(string sub)
     {
@@ -69,19 +68,19 @@ namespace TalStromApi.Controllers
         {
           return NotFound();
         }
-        
+
         var filterKey = user
           .Technologies.Split(',')
           .ToList();
-        
+
         var filteredUsers = await _context.User.Include(ctx => ctx.Posts)
           .Include(ctx => ctx.Videos)
           .Include(ctx => ctx.Images)
           .Where(user => user.Role == "developer")
-          .Where(user => filterKey.Any(elm => user.Technologies.Contains(elm)&& user.Sub != sub))
+          .Where(user => filterKey.Any(elm => user.Technologies.Contains(elm) && user.Sub != sub))
           .ToListAsync();
-        
-        return  Ok(filteredUsers);
+
+        return Ok(filteredUsers);
       }
       catch (Exception ex)
       {
@@ -89,7 +88,7 @@ namespace TalStromApi.Controllers
         return StatusCode(500, "Internal Server Error");
       }
     }
-    
+
     [HttpGet("role/{role}")]
     public async Task<IActionResult> GetUsersByRole(string role)
     {
@@ -103,45 +102,46 @@ namespace TalStromApi.Controllers
     [HttpPatch("{sub}/{role}")]
     public async Task<IActionResult> UpdateUserRole(string sub, string role)
     {
-      var user = await _context.User.FirstOrDefaultAsync(x=> x.Sub == sub);
+      var user = await _context.User.FirstOrDefaultAsync(x => x.Sub == sub);
 
       if (user is null) return NotFound();
 
-      user.Role = role ;
+      user.Role = role;
       await _context.SaveChangesAsync();
       return Ok(user);
     }
-    
+
     [HttpPatch("{sub}/activeStatus/{isActive}")]
     public async Task<IActionResult> UpdateUserActiveStatus(string sub, bool isActive)
     {
-      var user = await _context.User.FirstOrDefaultAsync(x=> x.Sub == sub);
+      var user = await _context.User.FirstOrDefaultAsync(x => x.Sub == sub);
 
       if (user is null) return NotFound();
 
-      user.Active = isActive ;
+      user.Active = isActive;
       await _context.SaveChangesAsync();
       return Ok(user);
     }
-    
-    [HttpPatch("editProfile/{sub}")]
+
+    [HttpPatch("editProfile/")]
     public async Task<ActionResult<User>> EditUserProfile(string sub, EditUserDTO editUserDto)
     {
-      var user = await _context.User.FirstOrDefaultAsync(x=> x.Sub == sub);
+      var user = await _context.User.FirstOrDefaultAsync(x => x.Sub == sub);
 
       if (user is null) return NotFound();
-      
+
       user.UserName = editUserDto.UserName;
-      user.Technologies = editUserDto.Technologies; 
+      user.Technologies = editUserDto.Technologies;
       user.Bio = editUserDto.Bio;
       user.Position = editUserDto.Position;
       user.LastModified = DateTime.Now;
 
       _context.User.Update(user);
       await _context.SaveChangesAsync();
-      return Ok(user);;
+      return Ok(user);
+      ;
     }
-    
+
     [HttpPost]
     public async Task<ActionResult<User>> PostUser(UserPostReq userReq)
     {
@@ -152,7 +152,7 @@ namespace TalStromApi.Controllers
         {
           return StatusCode(409, existingUser);
         }
-        
+
         var user = new User
         {
           Name = userReq.Name,
@@ -160,7 +160,7 @@ namespace TalStromApi.Controllers
           Picture = userReq.Picture,
           Sub = userReq.Sub
         };
-        
+
         _context.User.Add(user);
         await _context.SaveChangesAsync();
 
@@ -180,10 +180,10 @@ namespace TalStromApi.Controllers
       {
         return BadRequest();
       }
-      
+
       try
       {
-        var user = await _context.User.FirstOrDefaultAsync(x=> x.Sub == sub);
+        var user = await _context.User.FirstOrDefaultAsync(x => x.Sub == sub);
         if (user == null)
         {
           return NotFound();
@@ -200,9 +200,75 @@ namespace TalStromApi.Controllers
         return StatusCode(500, "Internal Server Error");
       }
     }
+
     private bool UserExists(string sub)
     {
       return _context.User.Any(e => e.Sub == sub);
+    }
+
+    [HttpGet("favorites")]
+    public async Task<ActionResult<IEnumerable<User>>> GetAllFavoriteUsers([FromQuery] string sub)
+    {
+      try
+      {
+        var user = await _context.User.FirstOrDefaultAsync(u => u.Sub == sub);
+
+        if (user == null)
+        {
+          return NotFound("User not found");
+        }
+
+        if (user.Favorites == null || user.Favorites.Count == 0)
+        {
+          return Ok("No favorite users");
+        }
+
+        var favoriteUsers = await _context.User.Where(u => user.Favorites.Contains(user.Sub)).ToListAsync();
+        return favoriteUsers;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in GetAllFavoriteUsers: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+
+    [HttpPatch("favorite")]
+    public async Task<IActionResult> ToggleFavoriteUser(string sub)
+    {
+      try
+      {
+        var user = await _context.User.FirstOrDefaultAsync(u => u.Sub == sub);
+
+        if (user == null)
+        {
+          return BadRequest("Invalid user");
+        }
+
+        if (user.Favorites == null)
+        {
+          user.Favorites = new List<string>();
+        }
+
+        if (user.Favorites.Contains(sub))
+        {
+          // If the user is already a favorite, remove them
+          user.Favorites.Remove(sub);
+        }
+        else
+        {
+          // If the user is not a favorite, add them
+          user.Favorites.Add(sub);
+        }
+
+        await _context.SaveChangesAsync();
+        return NoContent();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error in ToggleFavoriteUser: {ex}");
+        return StatusCode(500, "Internal Server Error");
+      }
     }
   }
 }
