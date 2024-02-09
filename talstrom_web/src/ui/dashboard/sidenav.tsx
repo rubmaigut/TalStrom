e2b4d478b8600c8beb32d44e0439c828e08a5d35
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-09 09:24:07 +0100

    fix sideNavButton

--- talstrom_web/src/ui/dashboard/sidenav.tsx
+++ talstrom_web/src/ui/dashboard/sidenav.tsx
@@ -9,7 +9,7 @@ export default function SideNav() {
   const { userContextG } = useUser();
 
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex w-full h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-16 items-center justify-center rounded-md bg-gray-50 p-4 md:h-30"
         href="/"
@@ -23,8 +23,8 @@ export default function SideNav() {
           priority
         />
       </Link>
-      <div className="flex grow flex-row items-center justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        {userContextG?.role === "admin" ? <NavLinks /> : <CustomerNavLinks id />}
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        {userContextG?.role === "admin" ? <NavLinks /> : <CustomerNavLinks/>}
         <div className="hidden h-auto w-full grow rounded-md bg-gray-100 md:block"></div>
         <LogoutButton/>
       </div>
