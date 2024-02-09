420e67cf9fc274e1877e2178b360750190e39217
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-09 04:09:42 +0100

    post ui

--- talstrom_web/src/ui/atoms/profile/posts.tsx
+++ talstrom_web/src/ui/atoms/profile/posts.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
-import PostOverlay from '@/ui/overlays/post-viewer';
-import AddPostOverlay from '@/ui/overlays/add-post-overlay';
-import { addNewPostHandler, deleteUserPost, updateUserPost } from '@/lib/data-post';
-import { Session } from 'next-auth';
+import React, { useState } from "react";
+import PostOverlay from "@/ui/overlays/post-viewer";
+import AddPostOverlay from "@/ui/overlays/add-post-overlay";
+import {
+  addNewPostHandler,
+  deleteUserPost,
+  updateUserPost,
+} from "@/lib/data-post";
+import { Session } from "next-auth";
+import Image from "next/image";
 
 interface PostsProps {
   postType?: string;
   sub: string;
   posts: Post[];
-  session: Session | null
+  session: Session | null;
 }
 
 const UserPost: React.FC<PostsProps> = ({ posts, sub, session }) => {
   const [selectedPost, setSelectedPost] = useState<Post | null>(null);
   const [isEditMode, setEditMode] = useState<boolean>(false);
-  const [editedTitle, setEditedTitle] = useState<string>('');
-  const [editedContent, setEditedContent] = useState<string>('');
+  const [editedTitle, setEditedTitle] = useState<string>("");
+  const [editedContent, setEditedContent] = useState<string>("");
   const [isAddPostMode, setAddPostMode] = useState<boolean>(false);
 
   const handlePostEditClick = (post: Post) => {
@@ -25,9 +30,13 @@ const UserPost: React.FC<PostsProps> = ({ posts, sub, session }) => {
     setEditMode(true);
   };
 
-  const handleAddPost = async (title: string, content: string, postType: string) => {
+  const handleAddPost = async (
+    title: string,
+    content: string,
+    postType: string
+  ) => {
     try {
-      if (content.trim() !== '') {
+      if (content.trim() !== "") {
         await addNewPostHandler(title, content, sub, postType);
         setAddPostMode(false);
       }
@@ -51,13 +60,13 @@ const UserPost: React.FC<PostsProps> = ({ posts, sub, session }) => {
         setEditMode(false);
         setSelectedPost(null);
       } catch (error) {
-        console.error('Failed to update post', error);
+        console.error("Failed to update post", error);
       }
     }
   };
 
   const handlePostCancelClick = () => {
-    setEditedContent(selectedPost?.content || '');
+    setEditedContent(selectedPost?.content || "");
     setEditMode(false);
     setSelectedPost(null);
   };
@@ -78,34 +87,45 @@ const UserPost: React.FC<PostsProps> = ({ posts, sub, session }) => {
         setEditMode(false);
         setSelectedPost(null);
       } catch (error) {
-        console.error('Failed to delete post', error);
+        console.error("Failed to delete post", error);
       }
     }
   };
 
   return (
-    <div className="w-full py-4">
-      <div className='flex '>
-      <h2 className="text-2xl font-bold text-gray-600 pr-8">Post</h2>
-      <button className="w-6 rounded-full bg-teal-500 p-2 text-white float-left mb-3" onClick={handleAddPostClick}> + </button>
+    <div className="w-full flex flex-col items-center space-x-3">
+      <div className="w-full justify-between flex items-center flex-1 mb-4">
+        <Image
+          src={`${session?.user?.image}`} 
+          alt={`Profile ${session?.user?.name}`}
+          width={100}
+          height={100}
+          className="h-10 w-10 rounded-full"
+        />
+        <button
+          className="flex-1 ml-4 px-4 py-2 bg-gray-100 border border-gray-300 rounded-full text-left text-gray-400 hover:bg-gray-100"
+          onClick={handleAddPostClick}
+        >
+          Start a post
+        </button>
       </div>
 
       {posts.map((post) => (
         <div
           key={post.id}
           className={`w-full p-4 border rounded-md ${
-            post.postType === 'JobPost'
-              ? 'bg-teal-100'
-              : post.postType === 'Thoughts'
-              ? 'bg-green-100'
-              : 'bg-white-100'
+            post.postType === "JobPost"
+              ? "bg-teal-100"
+              : post.postType === "Thoughts"
+              ? "bg-green-100"
+              : "bg-white-100"
           }`}
           onClick={() => handlePostEditClick(post)}
         >
           <p className="text-xl font-bold mb-2">{post.title}</p>
           <p>{post.content}</p>
           <div className="mt-4 flex items-center justify-between">
-            {post.author && post.author !== 'string' && (
+            {post.author && post.author !== "string" && (
               <p className="text-gray-600 text-xs">{post.author}</p>
             )}
             <p className="text-gray-600 text-xs">{post.createdAt}</p>
@@ -130,7 +150,9 @@ const UserPost: React.FC<PostsProps> = ({ posts, sub, session }) => {
 
       {isAddPostMode && (
         <AddPostOverlay
-          onAddPost={(title, content, postType) => handleAddPost(title, content, postType)}
+          onAddPost={(title, content, postType) =>
+            handleAddPost(title, content, postType)
+          }
           onCancelClick={handleAddPostCancelClick}
         />
       )}
--- talstrom_web/src/ui/overlays/add-post-overlay.tsx
+++ talstrom_web/src/ui/overlays/add-post-overlay.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Image from "next/image";
+import session from "@/pages/api/endpoints/session";
+import { useSession } from "next-auth/react";
 
 interface AddPostOverlayProps {
   onAddPost: (title: string, content: string, postType: string) => void;
@@ -9,85 +12,117 @@ const AddPostOverlay: React.FC<AddPostOverlayProps> = ({
   onAddPost,
   onCancelClick,
 }) => {
-  const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-  const [postType, setPostType] = useState<string>('Thought');
+  const { data: session } = useSession();
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [postType, setPostType] = useState<string>("Thought");
 
   const handleAddPostClick = () => {
     onAddPost(title, content, postType);
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-md w-96">
-        <h2 className="text-2xl font-bold mb-4">Add New Post</h2>
-
-        <div className="mb-4">
-          <label
-            htmlFor="postType"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Post Type
-          </label>
-          <select
-            id="postType"
-            name="postType"
-            className="mt-1 p-2 border rounded-md w-full"
-            value={postType}
-            onChange={(e) => setPostType(e.target.value)}
-          >
-            <option value="Thought">Thought</option>
-            <option value="Challenge">Challenge</option>
-          </select>
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="title"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Title
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            className="mt-1 p-2 border rounded-md w-full"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="content"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Content
-          </label>
-          <textarea
-            id="content"
-            name="content"
-            rows={4}
-            className="mt-1 p-2 border rounded-md w-full"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-
-        <div className="flex justify-end">
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-start justify-center p-4 overflow-y-auto">
+      <div className="bg-white rounded-lg w-full max-w-lg mx-auto mt-20 p-6">
+        <div className="flex flex-col items-center justify-between border-b pb-3">
+          <div className="flex justify-between items-center">
+            <Image
+              src={`${session?.user?.image}`}
+              alt="Profile"
+              width={100}
+              height={100}
+              className=" h-10 w-10 rounded-full mr-3"
+            />
+            <div>
+              <div className="font-medium text-lg">{session?.user?.name}</div>
+              <div className="text-gray-500 text-sm">Post to Anyone</div>
+            </div>
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
-            onClick={handleAddPostClick}
-          >
-            Add Post
-          </button>
-          <button
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md"
+            className="rounded-full p-2 text-gray-500 hover:bg-gray-200 transition duration-150"
             onClick={onCancelClick}
           >
-            Cancel
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
           </button>
+          </div>
+          <div className="w-full my-4">
+            <label
+              htmlFor="postType"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Post Type
+            </label>
+            <select
+              id="postType"
+              name="postType"
+              className="mt-1 p-2 border rounded-md w-full"
+              value={postType}
+              onChange={(e) => setPostType(e.target.value)}
+            >
+              <option value="Thought">Thought</option>
+              <option value="Challenge">Challenge</option>
+            </select>
+          </div>
+          <div className="w-full my-4">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="mt-1 p-2 border rounded-md w-full"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+
+          <div className="w-full my-4">
+            <label
+              htmlFor="content"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Content
+            </label>
+            <textarea
+              id="content"
+              name="content"
+              rows={4}
+              className="mt-1 p-2 border rounded-md w-full"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+
+          <div className="flex justify-end">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+              onClick={handleAddPostClick}
+            >
+              Add Post
+            </button>
+            <button
+              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md"
+              onClick={onCancelClick}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </div>
