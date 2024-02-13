1c58528753039bbcbebe5a88d801d48e46917125
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-13 07:09:37 +0100

    update add fav

--- talstrom_web/src/lib/data-user.ts
+++ talstrom_web/src/lib/data-user.ts
@@ -1,23 +1,23 @@
-import { User } from '@/types/IUser';
-import { EditUserProfile } from '@/ui/atoms/profile/edit-profile';
+import { User } from "@/types/IUser";
+import { EditUserProfile } from "@/ui/atoms/profile/edit-profile";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function addUserHandler(user: User): Promise<User> {
   const url = `${API_BASE_URL}/Users`;
   const response = await fetch(url, {
-    cache: 'no-store',
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    cache: "no-store",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       name: user.name,
       email: user.email,
       picture: user.picture,
       sub: user.sub,
-      role: user.role
+      role: user.role,
     }),
   });
-  if (!response.ok) throw new Error('addUserHandler: Failed to Create user');
+  if (!response.ok) throw new Error("addUserHandler: Failed to Create user");
   return await response.json();
 }
 
@@ -28,20 +28,20 @@ export async function fetchUsers() {
       revalidate: 60,
     },
   });
-  if (!response.ok) throw new Error('fetchUsersByRole: Failed to fetch users');
+  if (!response.ok) throw new Error("fetchUsersByRole: Failed to fetch users");
   return await response.json();
 }
 
 export async function fetchUsersBySub(sub: string) {
   const url = `${API_BASE_URL}/Users/${sub}`;
   const response = await fetch(url, {
-    cache: 'no-store',
+    cache: "no-store",
   });
   if (!response.ok) {
     if (response.status === 404) {
       return null;
     }
-    throw new Error('fetchUsersByRole: Failed to fetch users');
+    throw new Error("fetchUsersByRole: Failed to fetch users");
   }
   return await response.json();
 }
@@ -49,57 +49,71 @@ export async function fetchUsersBySub(sub: string) {
 export async function fetchUsersByRole(role: string) {
   const url = `${API_BASE_URL}/Users/role/${role}`;
   const response = await fetch(url, {
-    cache: 'no-store',
+    cache: "no-store",
   });
-  if (!response.ok) throw new Error('fetchUsersByRole: Failed to fetch users');
+  if (!response.ok) throw new Error("fetchUsersByRole: Failed to fetch users");
   return await response.json();
 }
 
 export async function fetchUsersByFilter(sub: string) {
   const url = `${API_BASE_URL}/Users/technologies/filter/${sub}/`;
   const response = await fetch(url, {
-    cache: 'no-store',
-    headers: { 'Content-Type': 'application/json' },
+    cache: "no-store",
+    headers: { "Content-Type": "application/json" },
   });
-  if (!response.ok) throw new Error('fetchUsersByRole: Failed to fetch users');
+  if (!response.ok) throw new Error("fetchUsersByRole: Failed to fetch users");
   return await response.json();
 }
 
 export async function updateUserRole(sub: string, role: string) {
   const url = `${API_BASE_URL}/Users/${sub}/${role}`;
   const response = await fetch(url, {
-    cache: 'no-store',
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
+    cache: "no-store",
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
   });
   if (!response.ok)
-    throw new Error('updateUserRole:Failed to update user role');
+    throw new Error("updateUserRole:Failed to update user role");
   return await response.json();
 }
 
 export async function updateUserProfile(sub: string, props: EditUserProfile) {
   const adjustedProps = {
     ...props,
-    technologies: props.technologies.join(','),
+    technologies: props.technologies.join(","),
   };
   const url = `${API_BASE_URL}/Users/editProfile/${sub}`;
   const response = await fetch(url, {
-    cache: 'no-store',
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
+    cache: "no-store",
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(adjustedProps),
   });
   if (!response.ok)
-    throw new Error('updateUserRole:Failed to update user role');
+    throw new Error("updateUserRole:Failed to update user role");
   return await response.json();
+}
+
+export async function addFavorite(currentUserSub: string, favoriteSub: string) {
+  const url = `${API_BASE_URL}/Users/favorite/toggle?userSub=${currentUserSub}&favoriteSub=${favoriteSub}}`;
+
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Error: ${response.statusText}`);
+  }
 }
 
 export async function deleteUser(sub: string) {
   const response = await fetch(`${API_BASE_URL}/Users/${sub}`, {
-    method: 'DELETE',
+    method: "DELETE",
   });
   if (!response.ok) {
-    throw new Error('deleteUser:It could not delete the developer');
+    throw new Error("deleteUser:It could not delete the developer");
   }
   return response.json();
 }
